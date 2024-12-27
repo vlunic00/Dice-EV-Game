c85315077a1518fb8a967098bc1a30b07b01806a
vlunic00 <vlunic00@fesb.hr>
2024-12-27 19:54:01 +0100

    Added function to calculate the expected average roll after a new dice roll

--- diceSimulation.py
+++ diceSimulation.py
@@ -1,23 +1,36 @@
 import numpy as np
+import random
 
 DIE = [1, 2, 3, 4, 5, 6]
 
 
-def calculateExpectedPayout(ep, throws_left):
+def calculateExpectedPayout(ep, rolls_left):
     reroll = []
-    if ep[throws_left] == 0:
+    if ep[rolls_left] == 0:
         for value in DIE:
-            if value > calculateExpectedPayout(ep, throws_left - 1):
+            if value > calculateExpectedPayout(ep, rolls_left - 1):
                 reroll.append(value)
-        ep[throws_left] = (len(reroll) / 6) * (np.mean(reroll)) + (1 - len(reroll) / 6) * ep[throws_left - 1]
-        return ep[throws_left]
+        ep[rolls_left] = (len(reroll) / 6) * (np.mean(reroll)) + (1 - len(reroll) / 6) * ep[rolls_left - 1]
+        return ep[rolls_left]
 
-    return ep[throws_left]
+    return ep[rolls_left]
+
+
+def simulateRolls(diceRolls, rolls_left):
+    for roll in range(rolls_left):
+        diceRolls.append(random.choice(DIE))
+
+def calculateExpectedNewAverage(diceRolls, rolls):
+    return (sum(diceRolls) + ep[-rolls]) / (rolls + 1)
+
 
 
 if __name__ == "__main__":
-    EP_array = np.zeros(20)
+    EP_array = np.zeros(50)
     EP_array[0] = 3.5
-    throws_left = 19
-    calculateExpectedPayout(EP_array, throws_left)
+    diceRolls = []
+    rolls_left = 50
+    calculateExpectedPayout(EP_array, rolls_left - 1)
+    simulateRolls(diceRolls, rolls_left)
+    print(diceRolls)
     print(EP_array)

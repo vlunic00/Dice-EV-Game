e188e1a96688fcbecaae7e5aee4afc2616e91047
vlunic00 <vlunic00@fesb.hr>
2025-01-02 20:21:48 +0100

    Improved the expected average roll function

--- diceSimulation.py
+++ diceSimulation.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 
+np.set_printoptions(legacy='1.25')
+
 DIE = [1, 2, 3, 4, 5, 6]
 
 
@@ -16,12 +18,13 @@ def calculateExpectedPayout(ep, rolls_left):
     return ep[rolls_left]
 
 
-def simulateRolls(diceRolls, rolls_left):
-    for roll in range(rolls_left):
-        diceRolls.append(random.choice(DIE))
+def rollDice(ep, roll, diceRolls, current_average, expected_new_average):
+    diceRolls.append(random.choice(DIE))
+    expected_new_average.append(calculateExpectedNewAverage(diceRolls, roll, ep))
+    current_average.append(sum(diceRolls) / (roll + 1))
 
-def calculateExpectedNewAverage(diceRolls, rolls):
-    return (sum(diceRolls) + ep[-rolls]) / (rolls + 1)
+def calculateExpectedNewAverage(diceRolls, roll, ep):
+    return (sum(diceRolls) + ep[roll + 1]) / (roll + 2)
 
 
 
@@ -29,8 +32,18 @@ if __name__ == "__main__":
     EP_array = np.zeros(50)
     EP_array[0] = 3.5
     diceRolls = []
+    current_average = []
+    expected_new_average = []
     rolls_left = 50
     calculateExpectedPayout(EP_array, rolls_left - 1)
-    simulateRolls(diceRolls, rolls_left)
-    print(diceRolls)
-    print(EP_array)
+    for roll in range(rolls_left - 1):
+        rollDice(EP_array, roll, diceRolls, current_average, expected_new_average)
+        if current_average[roll - 1] > expected_new_average[roll - 1]:
+            print(f"{current_average}\n")
+            print(expected_new_average)
+            break
+            
+    print(f"{EP_array}\n")
+    print(f"{diceRolls}\n")
+    print(f"{current_average}\n")
+    print(expected_new_average)

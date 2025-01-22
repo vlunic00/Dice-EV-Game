0a834223810bace7fc1b41a30dafea29a0dc6b61
vlunic00 <vlunic00@fesb.hr>
2025-01-22 17:19:47 +0100

    Saved graph of the Simulation + added legend to graph

--- diceSimulation.py
+++ diceSimulation.py
@@ -33,7 +33,7 @@ def updateGlobalAverages(global_average, expected_global_average, n, roll, curre
     expected_global_average[roll] = (expected_global_average[roll] * (n - 1) + expected_new_average[roll]) / n
 
 
-def runSimulation(global_average, expected_global_average, n):
+def runSimulation(global_average, expected_global_average, n, tresholds):
     EP_array = np.zeros(50)
     EP_array[0] = 3.5
     diceRolls = []
@@ -44,6 +44,10 @@ def runSimulation(global_average, expected_global_average, n):
     for roll in range(rolls_left - 1):
         rollDice(EP_array, roll, diceRolls, current_average, expected_new_average)
         updateGlobalAverages(global_average, expected_global_average, n, roll, current_average, expected_new_average)
+        if current_average[roll] > expected_new_average[roll]:
+            tresholds.append(current_average[roll])
+            break
+
 
 
 
@@ -52,11 +56,14 @@ if __name__ == "__main__":
     number_of_simulations = 10000
     global_average = np.zeros(49)
     expected_global_average = np.zeros(49)
-    average = 0
+    tresholds = []
     for n in range(1, number_of_simulations + 1):
-        runSimulation(global_average, expected_global_average, n)
+        runSimulation(global_average, expected_global_average, n, tresholds)
     plt.plot(np.arange(1, 50), global_average)
     plt.plot(np.arange(1, 50), expected_global_average)
+    plt.legend(["Global Average Roll", "Expected Global Average Roll"])
     plt.xlabel("Number of rolls")
     plt.ylabel("Average roll value until roll")
     plt.show()
+    print(min(tresholds))
+    print(f"{len(tresholds) / number_of_simulations * 100}%")
